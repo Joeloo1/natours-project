18cd79ef21764ad3c419566307b6152dd0894c29
joeloo1 <agboolajoel123@gmail.com>
2026-01-26 21:51:54 +0100

    feat: add Stripe checkout session for tour bookings

---
+++ controllers/bookingController.js
@@ -0,0 +1,42 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.tourId);
+
+  const session = await stripe.checkout.sessions.create({
+    mode: 'payment',
+
+    payment_method_types: ['card'],
+
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: tour.name,
+            description: tour.summary,
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+            ],
+          },
+        },
+        quantity: 1,
+      },
+    ],
+  });
+
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
