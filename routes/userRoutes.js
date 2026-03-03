2238d458959fb39275411d7bd045a00808713dbf
Joeloo1 <agboolajoel123@gmail.com>
2026-03-03 11:45:18 +0100

    feat(user): add getUser controller to user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,39 +1,39 @@
-const express = require('express');
-const userController = require('./../controllers/userController');
-const authController = require('./../controllers/authController');
+const express = require("express");
+const userController = require("./../controllers/userController");
+const authController = require("./../controllers/authController");
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-router.get('/logout', authController.logOut);
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+router.get("/logout", authController.logOut);
 
-router.post('/forgetPassword', authController.forgotPassword);
-router.patch('/resetPassword/:token', authController.resetPassword);
+router.post("/forgetPassword", authController.forgotPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
 
 // Protect all routes after this middleware
 router.use(authController.protect);
 
-router.patch('/updateMyPassword', authController.updatePassword);
-router.get('/me', userController.getMe);
+router.patch("/updateMyPassword", authController.updatePassword);
+router.get("/me", userController.getMe, userController.getUser);
 router.patch(
-  '/updateMe',
+  "/updateMe",
   userController.uploadUserPhoto,
   userController.resizeUserPhoto,
   userController.updateMe,
 );
-router.delete('/deleteMe', userController.deleteMe);
+router.delete("/deleteMe", userController.deleteMe);
 
 // Restrict all routes after this middleware to admin only
-router.use(authController.restrictTo('admin'));
+router.use(authController.restrictTo("admin"));
 
 router
-  .route('/')
+  .route("/")
   .get(userController.getAllUser)
   .post(userController.createUser);
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
